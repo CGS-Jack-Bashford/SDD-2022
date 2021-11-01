5215df915716e82f3611d5559383ed656b440d9d
CGS-Jack-Bashford <37096442+CGS-Jack-Bashford@users.noreply.github.com>
2021-11-01 21:02:24 +1100

    String Activities

---
+++ Activities/9.2.2/StringValidationRegex/Form1.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Text.RegularExpressions
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ValidateCode()
+
+    End Sub
+
+    Private Sub ValidateCode()
+
+        Dim code As String
+        Dim pattern As Regex = New Regex("^[A-HJ-NP]\d{3}[A-HJ-NP]")
+
+        code = InputBox("Code: ")
+
+        While code <> "AMG"
+
+            If pattern.IsMatch(code) Then
+
+                MsgBox("Valid Code!")
+
+            Else
+
+                MsgBox("Invalid Code :(")
+
+            End If
+
+            code = InputBox("Code: ")
+
+        End While
+
+        Me.Close()
+
+    End Sub
+
+End Class
