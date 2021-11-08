8b606df98663c5a26d81fd7328df70d13c166e8f
Jack Bashford <jack.bashford42@gmail.com>
2021-11-08 18:15:50 +1100

    Relative file records

---
+++ Activities/9.2.2/RelativeFilesRecords/Form1.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cmdHarderWriteDemo = New System.Windows.Forms.Button()
+        Me.cmdHarderReadDemo = New System.Windows.Forms.Button()
+        Me.lstOutput = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'cmdHarderWriteDemo
+        '
+        Me.cmdHarderWriteDemo.Location = New System.Drawing.Point(540, 74)
+        Me.cmdHarderWriteDemo.Name = "cmdHarderWriteDemo"
+        Me.cmdHarderWriteDemo.Size = New System.Drawing.Size(75, 23)
+        Me.cmdHarderWriteDemo.TabIndex = 0
+        Me.cmdHarderWriteDemo.Text = "Button1"
+        Me.cmdHarderWriteDemo.UseVisualStyleBackColor = True
+        '
+        'cmdHarderReadDemo
+        '
+        Me.cmdHarderReadDemo.Location = New System.Drawing.Point(540, 104)
+        Me.cmdHarderReadDemo.Name = "cmdHarderReadDemo"
+        Me.cmdHarderReadDemo.Size = New System.Drawing.Size(75, 23)
+        Me.cmdHarderReadDemo.TabIndex = 0
+        Me.cmdHarderReadDemo.Text = "Button2"
+        Me.cmdHarderReadDemo.UseVisualStyleBackColor = True
+        '
+        'lstOutput
+        '
+        Me.lstOutput.FormattingEnabled = True
+        Me.lstOutput.ItemHeight = 15
+        Me.lstOutput.Location = New System.Drawing.Point(12, 12)
+        Me.lstOutput.Name = "lstOutput"
+        Me.lstOutput.Size = New System.Drawing.Size(469, 424)
+        Me.lstOutput.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lstOutput)
+        Me.Controls.Add(Me.cmdHarderReadDemo)
+        Me.Controls.Add(Me.cmdHarderWriteDemo)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents cmdHarderWriteDemo As Button
+    Friend WithEvents cmdHarderReadDemo As Button
+    Friend WithEvents lstOutput As ListBox
+End Class
---
+++ Activities/9.2.2/RelativeFilesRecords/Form1.vb
@@ -0,0 +1,50 @@
+﻿Public Class Form1
+
+    Public Structure recStudentInfo
+        Public skey As String
+        Public first As String
+        Public last As String
+    End Structure
+
+    Dim arrStudents(3) As recStudentInfo
+
+    Private Sub cmdHarderWriteDemo_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdHarderWriteDemo.Click
+        Dim counter As Integer
+
+        'set up data
+        arrStudents(1).skey = "SMITHJ"
+        arrStudents(1).last = "Smith"
+        arrStudents(1).first = "James"
+        arrStudents(2).skey = "WUJ"
+        arrStudents(2).last = "Wu"
+        arrStudents(2).first = "John"
+        arrStudents(3).skey = "JONESJ"
+        arrStudents(3).last = "Jones"
+        arrStudents(3).first = "Joshua"
+
+        'write to file
+        FileSystem.FileOpen(1, "textstudents.txt", OpenMode.Random)
+        FileSystem.FilePut(1, arrStudents(1))
+        FileSystem.FilePut(1, arrStudents(2))
+        FileSystem.FilePut(1, arrStudents(3))
+        FileSystem.FileClose(1)
+
+    End Sub
+
+    Private Sub cmdHarderReadDemo_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdHarderReadDemo.Click
+        FileSystem.FileOpen(1, "textstudents.txt", OpenMode.Random)
+        FileSystem.FileGet(1, arrStudents(1))
+        FileSystem.FileGet(1, arrStudents(2))
+        FileSystem.FileGet(1, arrStudents(3))
+        FileSystem.FileClose(1)
+
+        lstOutput.Items.Add("reading records from random file")
+        lstOutput.Items.Add("arrstudents(1).skey is " & arrStudents(1).skey)
+        lstOutput.Items.Add("arrstudents(3).last is " & arrStudents(3).last)
+
+        'you could also read from anywhere in the file, e.g.
+        'FileSystem.FileGet(1, arrStudents(3), 3) will just get the third record
+        'and place it in arrStudents(3).
+    End Sub
+
+End Class
