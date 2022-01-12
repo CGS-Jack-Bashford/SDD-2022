b4609e25f4ca0915bdef2f53bd441e25eff35833
Jack Bashford <jack.bashford42@gmail.com>
2022-01-13 10:18:37 +1100

    frmInstructions UI complete

--- Major Project/MazeGame/frmInstructions.Designer.vb
+++ Major Project/MazeGame/frmInstructions.Designer.vb
@@ -24,19 +24,81 @@ Partial Class frmInstructions
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmInstructions))
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
         Me.Label1.BackColor = System.Drawing.Color.Transparent
-        Me.Label1.Font = New System.Drawing.Font("Maze", 60.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.Label1.Location = New System.Drawing.Point(456, 20)
+        Me.Label1.Font = New System.Drawing.Font("Maze", 72.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(411, 20)
         Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(455, 54)
+        Me.Label1.Size = New System.Drawing.Size(547, 64)
         Me.Label1.TabIndex = 1
         Me.Label1.Text = "Instructions"
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Anonymous Pro for Powerline", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(337, 119)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(694, 104)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Choose a name (alphanumeric and underscores, 1-16 characters) and a difficulty (s" &
+    "tandard sizes of 10x10, 20x20, or 30x30, and a random choice of those options as" &
+    " well)"
+        '
+        'Label3
+        '
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Font = New System.Drawing.Font("Anonymous Pro for Powerline", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label3.Location = New System.Drawing.Point(337, 253)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(694, 107)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Then use arrow keys or the on—screen controls to navigate through the maze from t" &
+    "he starting square to the ending square. Try to get through the maze as fast as " &
+    "possible! "
+        '
+        'Label4
+        '
+        Me.Label4.BackColor = System.Drawing.Color.Transparent
+        Me.Label4.Font = New System.Drawing.Font("Anonymous Pro for Powerline", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label4.Location = New System.Drawing.Point(337, 390)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(694, 79)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "You can't move into a square blocked from your current square by a wall, and you " &
+    "can't move beyond the boundaries of the maze."
+        '
+        'Label5
+        '
+        Me.Label5.BackColor = System.Drawing.Color.Transparent
+        Me.Label5.Font = New System.Drawing.Font("Anonymous Pro for Powerline", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label5.Location = New System.Drawing.Point(337, 499)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(694, 77)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "Once you're through the maze, you can copy your seed to play the same maze and tr" &
+    "y for a faster time. "
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.White
+        Me.Button1.Font = New System.Drawing.Font("Anonymous Pro for Powerline", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button1.Location = New System.Drawing.Point(635, 642)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(98, 34)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Back"
+        Me.Button1.UseVisualStyleBackColor = False
         '
         'frmInstructions
         '
@@ -45,6 +107,11 @@ Partial Class frmInstructions
         Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
         Me.ClientSize = New System.Drawing.Size(1352, 729)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.DoubleBuffered = True
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
@@ -57,4 +124,9 @@ Partial Class frmInstructions
     End Sub
 
     Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Button1 As Button
 End Class
