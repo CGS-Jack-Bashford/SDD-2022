d747a58886c81ae170121670f8436350d4b6b72d
Jack Bashford <jack.bashford42@gmail.com>
2022-05-22 12:56:07 +1000

    highscore navigation

--- Major Project/MazeGame/frmHighscores.vb
+++ Major Project/MazeGame/frmHighscores.vb
@@ -7,6 +7,22 @@
 
         SetupUI()
 
+        Dim buttonList As Button() = {btnMazeSize10, btnMazeSize10Back, btnMazeSize20, btnMazeSize20Back, btnMazeSize30, btnMazeSize30Back}
+
+        For i = 0 To buttonList.Length - 1 Step 1
+
+            AddHandler buttonList(i).Click, AddressOf ChangeHighscoresViewBtn
+
+        Next i
+
+        Dim labelList As Label() = {lblMazeSize10, lblMazeSize20, lblMazeSize30}
+
+        For i = 0 To labelList.Length - 1 Step 1
+
+            AddHandler labelList(i).Click, AddressOf ChangeHighscoresViewLbl
+
+        Next
+
     End Sub
 
     Private Sub SetupUI()
@@ -25,9 +41,11 @@
 
         Next kvp
 
-        btnMazeSize10Back.BackColor = applicationColors("green")
-        btnMazeSize10Back.FlatAppearance.MouseOverBackColor = applicationColors("green")
-        btnMazeSize10Back.FlatAppearance.MouseDownBackColor = applicationColors("green")
+        btnMazeSize10.BackColor = applicationColors("green")
+        btnMazeSize10.FlatAppearance.MouseOverBackColor = applicationColors("green")
+        btnMazeSize10.FlatAppearance.MouseDownBackColor = applicationColors("green")
+
+        UpdateView(SelectedMazeSize)
 
     End Sub
 
@@ -43,9 +61,82 @@
 
     End Sub
 
-    Private Sub ChangeHighscoresView(sender As Object, e As EventArgs)
+    Private Sub ChangeHighscoresViewBtn(sender As Object, e As EventArgs)
 
+        Dim buttonClicked As Button = sender
+        Dim newMazeSize As Integer = CInt(buttonClicked.Name(11).ToString()) - 1
 
+        If newMazeSize <> SelectedMazeSize Then
+
+            SelectedMazeSize = newMazeSize
+            UpdateView(SelectedMazeSize)
+
+        End If
+
+    End Sub
+
+    Private Sub ChangeHighscoresViewLbl(sender As Object, e As EventArgs)
+
+        Dim labelClicked As Label = sender
+        Dim newMazeSize As Integer = CInt(labelClicked.Name(11).ToString()) - 1
+
+        If newMazeSize <> SelectedMazeSize Then
+
+            UpdateView(newMazeSize)
+            SelectedMazeSize = newMazeSize
+
+        End If
+
+    End Sub
+
+    Public Sub UpdateView(newMazeSize As Integer)
+
+        Dim highScoresToDisplay As Highscore() = arrHighscores(newMazeSize)
+
+        Dim rankingLabels As Label()() = New Label(4)() {}
+        rankingLabels(0) = {lblNameRanking1, lblTimeRanking1}
+        rankingLabels(1) = {lblNameRanking2, lblTimeRanking2}
+        rankingLabels(2) = {lblNameRanking3, lblTimeRanking3}
+        rankingLabels(3) = {lblNameRanking4, lblTimeRanking4}
+        rankingLabels(4) = {lblNameRanking5, lblTimeRanking5}
+
+        Dim currHighscore As Highscore
+
+        For i = 0 To 4 Step 1
+
+            If i < highScoresToDisplay.Length Then
+
+                currHighscore = highScoresToDisplay(i)
+                rankingLabels(i)(0).Text = currHighscore.playerName
+                rankingLabels(i)(1).Text = (currHighscore.gameTime \ 60).ToString().PadLeft(2, "0") & ":" & (currHighscore.gameTime Mod 60).ToString().PadLeft(2, "0")
+
+            Else
+
+                rankingLabels(i)(0).Text = ""
+                rankingLabels(i)(1).Text = ""
+
+            End If
+
+        Next i
+
+        UpdateButtonColor(newMazeSize)
+
+    End Sub
+
+    Public Sub UpdateButtonColor(newMazeSize As Integer)
+
+        Dim buttonList As Button() = {btnMazeSize10, btnMazeSize20, btnMazeSize30}
+        Dim colorList As Color() = {applicationColors("green"), applicationColors("yellow"), applicationColors("red")}
+
+        Dim selectedButton As Button = buttonList(newMazeSize)
+        Dim selectedColor As Color = colorList(newMazeSize)
+
+        selectedButton.BackColor = selectedColor
+        selectedButton.FlatAppearance.MouseDownBackColor = selectedColor
+        selectedButton.FlatAppearance.MouseOverBackColor = selectedColor
+
+        Dim oldButton As Button = buttonList(SelectedMazeSize)
+        Dim white As Color = Color.White
 
     End Sub
 
