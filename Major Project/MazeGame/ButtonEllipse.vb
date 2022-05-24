24a9140c254630d2d831970d99eda9417ac3e3ab
Jack Bashford <jack.bashford42@gmail.com>
2022-05-24 21:41:53 +1000

    docx

--- Major Project/MazeGame/ButtonEllipse.vb
+++ Major Project/MazeGame/ButtonEllipse.vb
@@ -1,10 +1,17 @@
 ﻿Imports System.Windows.Forms
 Imports System.Drawing.Drawing2D
 
+''' <summary>
+''' This class allows for a circular button to be drawn by rendering a circular ellipse as a mask for the button
+''' </summary>
 Public Class ButtonEllipse
 
     Inherits Button
 
+    ''' <summary>
+    ''' Draws an ellipse around the center of the button, with the button's specified dimensions
+    ''' </summary>
+    ''' <param name="pevent">The paint event arguments from the form that the control belongs to, which are passed to the default paint handler</param>
     Protected Overrides Sub OnPaint(pevent As PaintEventArgs)
 
         Dim newGraphicsPath As GraphicsPath = New GraphicsPath()
