09d7d7bf5deceacfcd8377988f609b32d6e1ee56
Jack Bashford <jack.bashford42@gmail.com>
2022-06-19 12:50:18 +1000

    stage 4

---
+++ Major Project/Stage4/Stage4/Form1.Designer.vb
@@ -0,0 +1,123 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.lstInputs = New System.Windows.Forms.ListBox()
+        Me.btnTest = New System.Windows.Forms.Button()
+        Me.lstOutput = New System.Windows.Forms.ListBox()
+        Me.lstExpected = New System.Windows.Forms.ListBox()
+        Me.lbl_title = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lstInputs
+        '
+        Me.lstInputs.FormattingEnabled = True
+        Me.lstInputs.ItemHeight = 15
+        Me.lstInputs.Location = New System.Drawing.Point(6, 30)
+        Me.lstInputs.Name = "lstInputs"
+        Me.lstInputs.Size = New System.Drawing.Size(148, 199)
+        Me.lstInputs.TabIndex = 0
+        '
+        'btnTest
+        '
+        Me.btnTest.Location = New System.Drawing.Point(566, 8)
+        Me.btnTest.Name = "btnTest"
+        Me.btnTest.Size = New System.Drawing.Size(75, 23)
+        Me.btnTest.TabIndex = 1
+        Me.btnTest.Text = "Run Test"
+        Me.btnTest.UseVisualStyleBackColor = True
+        '
+        'lstOutput
+        '
+        Me.lstOutput.FormattingEnabled = True
+        Me.lstOutput.ItemHeight = 15
+        Me.lstOutput.Location = New System.Drawing.Point(160, 30)
+        Me.lstOutput.Name = "lstOutput"
+        Me.lstOutput.Size = New System.Drawing.Size(120, 199)
+        Me.lstOutput.TabIndex = 2
+        '
+        'lstExpected
+        '
+        Me.lstExpected.FormattingEnabled = True
+        Me.lstExpected.ItemHeight = 15
+        Me.lstExpected.Location = New System.Drawing.Point(335, 30)
+        Me.lstExpected.Name = "lstExpected"
+        Me.lstExpected.Size = New System.Drawing.Size(120, 199)
+        Me.lstExpected.TabIndex = 3
+        '
+        'lbl_title
+        '
+        Me.lbl_title.AutoSize = True
+        Me.lbl_title.Location = New System.Drawing.Point(9, 8)
+        Me.lbl_title.Name = "lbl_title"
+        Me.lbl_title.Size = New System.Drawing.Size(62, 15)
+        Me.lbl_title.TabIndex = 4
+        Me.lbl_title.Text = "Input Data"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(335, 8)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(96, 15)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Expected Output"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(160, 8)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(72, 15)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Output Data"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lbl_title)
+        Me.Controls.Add(Me.lstExpected)
+        Me.Controls.Add(Me.lstOutput)
+        Me.Controls.Add(Me.btnTest)
+        Me.Controls.Add(Me.lstInputs)
+        Me.Name = "Form1"
+        Me.Text = "Testing - ValidateName"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lstInputs As ListBox
+    Friend WithEvents btnTest As Button
+    Friend WithEvents lstOutput As ListBox
+    Friend WithEvents lstExpected As ListBox
+    Friend WithEvents lbl_title As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Major Project/Stage4/Stage4/Form1.vb
@@ -0,0 +1,84 @@
+﻿Imports System.Text.RegularExpressions
+
+Public Class Form1
+
+    Structure Highscore
+        Public gameTime As Integer
+        Public playerName As Integer
+    End Structure
+
+    Public Sub SortHighscores(ByRef arrHighscores As Highscore()(), ByVal sizeToSort As Integer)
+        Dim last As Integer = arrHighscores(sizeToSort).Length - 1
+        Dim swapped As Boolean = True
+        Dim i As Integer = 0
+        While swapped
+            swapped = False
+            i = 0
+            While i < last
+                If arrHighscores(sizeToSort)(i).gameTime > arrHighscores(sizeToSort)(i + 1).gameTime Then
+                    Swap(arrHighscores, sizeToSort, i, i + 1)
+                    swapped = True
+                End If
+                i = i + 1
+            End While
+            last = last - 1
+        End While
+    End Sub
+    ' Swap was included as it's a dependency of this function
+    Public Sub Swap(arrHighscores As Highscore()(), size As Integer, idxA As Integer, idxB As Integer)
+        Dim temp As Highscore = arrHighscores(size)(idxA)
+        arrHighscores(size)(idxA) = arrHighscores(size)(idxB)
+        arrHighscores(size)(idxB) = temp
+    End Sub
+
+    Private Sub TestSortHighscores()
+
+        Dim arrHighscores As Highscore()() = New Highscore(2)() {}
+        arrHighscores(0) = New Highscore(5) {}
+        arrHighscores(1) = New Highscore(5) {}
+        arrHighscores(2) = New Highscore(5) {}
+
+#Region "Assignment of Data Set 1"
+
+        arrHighscores(0)(0).playerName = "Alfred"
+        arrHighscores(0, 0).gameTime = 10
+        arrHighscores(0, 1).playerName = "Bob"
+        arrHighscores(0, 1).gameTime = 20
+        arrHighscores(0, 2).playerName = "Cheryl"
+        arrHighscores(0, 2).gameTime = 30
+        arrHighscores(0, 3).playerName = "Damon"
+        arrHighscores(0, 3).gameTime = 40
+        arrHighscores(0, 4).playerName = "Edvin"
+        arrHighscores(0, 4).gameTime = 50
+        arrHighscores(1, 0).playerName = "Frank"
+        arrHighscores(1, 0).gameTime = 11
+        arrHighscores(1, 1).playerName = "George"
+        arrHighscores(1, 1).gameTime = 21
+        arrHighscores(1, 2).playerName = "Henry"
+        arrHighscores(1, 2).gameTime = 31
+        arrHighscores(1, 3).playerName = "Isaac"
+        arrHighscores(1, 3).gameTime = 41
+        arrHighscores(1, 4).playerName = "Jacob"
+        arrHighscores(1, 4).gameTime = 51
+        arrHighscores(2, 0).playerName = "Kevin"
+        arrHighscores(2, 0).gameTime = 12
+        arrHighscores(2, 1).playerName = "Ludwig"
+        arrHighscores(2, 1).gameTime = 22
+        arrHighscores(2, 2).playerName = "Monty"
+        arrHighscores(2, 2).gameTime = 32
+        arrHighscores(2, 3).playerName = "Nathan"
+        arrHighscores(2, 3).gameTime = 42
+        arrHighscores(2, 4).playerName = "Olivia"
+        arrHighscores(2, 4).gameTime = 52
+
+
+#End Region
+
+    End Sub
+
+    Private Sub btnTest_Click(sender As Object, e As EventArgs) Handles btnTest.Click
+
+        TestSortHighscores()
+
+    End Sub
+End Class
