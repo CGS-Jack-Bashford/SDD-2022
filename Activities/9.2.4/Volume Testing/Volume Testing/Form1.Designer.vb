0c37a8750ea5af304561cbac76b16253d6d8bad8
Jack Bashford <jack.bashford42@gmail.com>
2022-07-22 09:43:54 +1000

    9.2.4 volume testing activity

---
+++ Activities/9.2.4/Volume Testing/Volume Testing/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.lstOutput = New System.Windows.Forms.ListBox()
+        Me.btnRead = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lstOutput
+        '
+        Me.lstOutput.FormattingEnabled = True
+        Me.lstOutput.ItemHeight = 25
+        Me.lstOutput.Location = New System.Drawing.Point(12, 12)
+        Me.lstOutput.Name = "lstOutput"
+        Me.lstOutput.Size = New System.Drawing.Size(180, 129)
+        Me.lstOutput.TabIndex = 0
+        '
+        'btnRead
+        '
+        Me.btnRead.Location = New System.Drawing.Point(200, 16)
+        Me.btnRead.Name = "btnRead"
+        Me.btnRead.Size = New System.Drawing.Size(112, 34)
+        Me.btnRead.TabIndex = 1
+        Me.btnRead.Text = "Button1"
+        Me.btnRead.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnRead)
+        Me.Controls.Add(Me.lstOutput)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents lstOutput As ListBox
+    Friend WithEvents btnRead As Button
+End Class
---
+++ Activities/9.2.4/Volume Testing/Volume Testing/Form1.vb
@@ -0,0 +1,46 @@
+﻿Public Class Form1
+
+    Private Structure recStudent 'declaring a single record
+        Dim StudentID As Integer
+        Dim LastName As String
+        Dim FirstName As String
+        Dim DOB As Date
+    End Structure
+
+    Private arrrecStudent(101) As recStudent 'declaring an array of these records
+
+    Private Sub btnRead_Click(sender As System.Object, e As System.EventArgs) Handles btnRead.Click
+
+        'these steps are adapted from http://www.dreamincode.net/forums/topic/94609-reading-a-text-file-into-an-array-of-structs/. See post 5 in particular for an excellent explanation.
+        Dim i As Integer
+        Dim tempString As String
+
+        i = 0
+        FileSystem.FileOpen(1, "C:\Users\owner\Downloads\dataJan-13-2016.csv", OpenMode.Input) 'change your filepath
+
+        While Not EOF(1)
+            i = i + 1
+            tempString = FileSystem.LineInput(1) 'reads a whole line from the file and assigns it to the variable tempString
+            Dim str(3) As String
+            str = tempString.Split(",") 'this line is cool. It splits tempString into 4 parts (divided by the comma), and writes the 4 parts to str(0), str(1), str(2) and str(3)
+            arrrecStudent(i).StudentID = CInt(str(0)) 'CInt converts the student ID to an integer. e.g. "1234" to 1234
+            arrrecStudent(i).LastName = str(1)
+            arrrecStudent(i).FirstName = str(2)
+            arrrecStudent(i).DOB = CDate(str(3))
+
+        End While
+
+        FileSystem.FileClose(1)
+
+        For i = 1 To 100
+            lstOutput.Items.Add(i)
+            lstOutput.Items.Add(arrrecStudent(i).StudentID)
+            lstOutput.Items.Add(arrrecStudent(i).LastName)
+            lstOutput.Items.Add(arrrecStudent(i).FirstName)
+            lstOutput.Items.Add(arrrecStudent(i).DOB)
+            lstOutput.Items.Add("")
+        Next i
+
+    End Sub
+
+End Class
